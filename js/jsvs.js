9df485de9809c181f367d3880c016552801c397d
mateoirusta20-stack <mateoirusta20@gmail.com>
2026-02-24 16:58:38 -0300

    Add files via upload

---
+++ js/jsvs.js
@@ -0,0 +1,12 @@
+alert()
+
+//var nom=prompt("ingrese nombre hijita:");
+//var edad=parseInt(prompt("ingresa tu edad ps:"));
+//if (edad>=18)
+//{
+//    document.writeln(nom+" estas habilitado pvta");
+//}
+//else
+//{
+ //   document.writeln(nom+" no estas habilitado pvta");
+//}
